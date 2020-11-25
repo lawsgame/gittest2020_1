61271b278801964e6d9b7ea2ed4dc4ebe88f93f9
lawsgame <jfweets1@gmail.com>
2020-11-25 04:50:11 +0100

    commit 3

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello 2");
         System.out.println("commit 2");
+        System.out.println("commit 3");
     }
 }
